1186f3176b59409613d4a44e19d72ba67452600a
Iryna Hridnova <diukovairyna@ukr.net>
2022-10-28 18:56:41 +0300

    app 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const imageContainer = document.querySelector(".gallery");
+const imageMarkup = createImageMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+imageContainer.addEventListener('click', onGalleryClick)
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createImageMarkup (galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return`<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img 
+        class="gallery__image"
+        src ="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>   
+        </div>`;
+    })
+    .join('');
+}   
+
+ 
+function onGalleryClick (event){
+    event.preventDefault()
+}
+
+
+
+
